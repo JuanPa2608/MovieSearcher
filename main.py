d5735d349c8256e1cb124776fae0e441776c36a9
JuanPa2608 <99778360+JuanPa2608@users.noreply.github.com>
2023-10-30 20:51:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
@@ -6,7 +7,10 @@ app = FastAPI()
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola"
-    
+
+
+df_PlayTimeGenre.read_parquet('..\\Funciones\\datasets\\PlayTimeGenre.parquet',index=False)
+
 @app.get("/PlayTimeGenre")
 async def PlayTimeGenre(genre):
     anio = df_PlayTimeGenre['release_year'][df_PlayTimeGenre['Sum_playtime_forever'][df_PlayTimeGenre['genres'] == genre].idxmax()]
