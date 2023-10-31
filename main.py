81ed5670261027d5b38f95afbfbefde36c082212
JuanPa2608 <99778360+JuanPa2608@users.noreply.github.com>
2023-10-30 20:38:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = FastAPI()
 @app.get("/inicio")
 async def ruta_prueba():
     return "Hola"
-
-def PlayTimeGenre(genre):
+    
+@app.get("/PlayTimeGenre")
+async def PlayTimeGenre(genre):
     anio = df_PlayTimeGenre['release_year'][df_PlayTimeGenre['Sum_playtime_forever'][df_PlayTimeGenre['genres'] == genre].idxmax()]
     return int(anio)
