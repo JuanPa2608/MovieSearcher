58296f45bd02a65359455839407851e5fde0889b
JuanPa2608 <99778360+JuanPa2608@users.noreply.github.com>
2023-10-31 07:50:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ df_PlayTimeGenre = pd.read_parquet('datasets/PlayTimeGenre.parquet')
 async def PlayTimeGenre(genre):
     anio = df_PlayTimeGenre['release_year'][df_PlayTimeGenre['Sum_playtime_forever'][df_PlayTimeGenre['genres'] == genre].idxmax()]
     return str('Año de lanzamiento con más horas jugadas para Género ' + genre + ': ' + str(anio))
+
+df_UserForGenre = pd.read_csv('datasets/UserForGenre.csv')
+
+@app.get("/UserForGenre")
+async def UserForGenre(genre):
+    lista = df_UserForGenre[genre]['user']
+    return str('Usuario con más horas jugadas para Género: ' + genre + ' Horas jugadas: ' + str(anio))
+
+
