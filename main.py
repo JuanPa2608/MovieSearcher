d01d524894291ceb863e6f7b4a4c6b564afde06a
JuanPa2608 <99778360+JuanPa2608@users.noreply.github.com>
2023-10-31 11:18:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ async def UsersRecommend(anio):
     #top_recomend = df_UsersRecommend[df_UsersRecommend['year_posted'] == anio]
     #top_recomend.reset_index(drop = True,inplace = True)
     #return str('Items_id Puesto 1: '+ top_recomend['item_id'][0] + ' Puesto 2: '+ top_recomend['item_id'][1] + ' Puesto 3: '+ top_recomend['item_id'][2])
-    return type(anio),anio
+    return str(type(anio))
     
 
