b86bd4921e8868d2204302d28ffc3e6ad19343b1
JuanPa2608 <99778360+JuanPa2608@users.noreply.github.com>
2023-11-01 16:59:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ app = FastAPI()
 async def ruta_prueba():
     return "Hola"
 
-df_PlayTimeGenre = pd.read_parquet('datasets/datasets/PlayTimeGenre.parquet')
+df_PlayTimeGenre = pd.read_parquet('Funciones/datasets/PlayTimeGenre.parquet')
 
 @app.get("/PlayTimeGenre")
 async def PlayTimeGenre(genre):
     anio = df_PlayTimeGenre['release_year'][df_PlayTimeGenre['Sum_playtime_forever'][df_PlayTimeGenre['genres'] == genre].idxmax()]
     return str('Año de lanzamiento con más horas jugadas para Género ' + genre + ': ' + str(anio))
 
-df_UserForGenre = pd.read_parquet('datasets/datasets/UserForGenre_total.parquet')
+df_UserForGenre = pd.read_parquet('Funciones/datasets/UserForGenre_total.parquet')
 
 @app.get("/UserForGenre")
 async def UserForGenre(genre):
@@ -23,7 +23,7 @@ async def UserForGenre(genre):
     lista = df_UserForGenre[genre][1]
     return str('Usuario con más horas jugadas para Género ' + genre + ': ' + user + ' Horas jugadas: ' + lista)
 
-df_UsersRecommend = pd.read_parquet('datasets/datasets/UsersRecommend.parquet')
+df_UsersRecommend = pd.read_parquet('Funciones/datasets/UsersRecommend.parquet')
 
 @app.get("/UsersRecommend")
 async def UsersRecommend(anio):
@@ -31,7 +31,7 @@ async def UsersRecommend(anio):
     top_recomend.reset_index(drop = True,inplace = True)
     return str('Items_id Puesto 1: '+ top_recomend['item_id'][0] + ' Puesto 2: '+ top_recomend['item_id'][1] + ' Puesto 3: '+ top_recomend['item_id'][2])
 
-df_UsersNotRecommend = pd.read_parquet('datasets/datasets/UsersNotRecommend.parquet')
+df_UsersNotRecommend = pd.read_parquet('Funciones/datasets/UsersNotRecommend.parquet')
 
 @app.get("/UsersNotRecommend")
 async def UsersNotRecommend(anio):
@@ -39,7 +39,7 @@ async def UsersNotRecommend(anio):
     top_not_recomend.reset_index(drop = True,inplace = True)
     return str('Items_id Puesto 1: '+ top_not_recomend['item_id'][0] + ' Puesto 2: '+ top_not_recomend['item_id'][1] + ' Puesto 3: '+ top_not_recomend['item_id'][2])
 
-df_SentimentRecommend = pd.read_parquet('datasets/datasets/SentimentRecommend.parquet')
+df_SentimentRecommend = pd.read_parquet('Funciones/datasets/SentimentRecommend.parquet')
 
 @app.get("/sentiment_analysis")
 async def sentiment_analysis(anio):
